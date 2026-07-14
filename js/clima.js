b71113c33ca4d4dbdbda90424a7ccff35cbd43d3
rubenjaramillo-hub <rubenjaramillo.c@gmail.com>
2026-07-13 19:20:24 -0500

    con clima

---
+++ js/clima.js
@@ -0,0 +1,35 @@
+const URL_CLIMA = "https://api.open-meteo.com/v1/forecast?latitude=-0.2298&longitude=-78.5258&hourly=temperature_2m";
+const btnClima= document.getElementById("btnClima");
+const contenedorClima = document.getElementById("listaTareas");
+const mensajeClima = document.getElementById("mensaje");
+async function obtenerClimaQuito() {
+    try {
+        mensajeClima.textContent = "Conectando con satelites metereologicos...";
+        contenedorClima.innerHTML = "";
+        let respuesta = await fetch(URL_CLIMA);
+        let datosConvertidos = await respuesta.json();
+        let climaActual = datosConvertidos.current_weather;
+        mensajeClima.textContent = " ";
+        const tarjetaClima = document.createElement("div");
+        tarjetaClima.classList.add("tarjeta-item");
+        let estadoCielo="Despejado o nubosidad variable";
+        if(climaActual.weathercode>=61 && climaActual.weathercode<=65) estadoCielo="Lluvia ligera a moderada";
+        if(climaActual.weathercode>=66 && climaActual.weathercode<=67) estadoCielo="Lluvia helada";
+        if(climaActual.weathercode>=71 && climaActual.weathercode<=77) estadoCielo="Nieve ligera a moderada";
+        tarjetaClima.innerHTML = `
+            <div style="display: flex; align-items: center; gap: 15px;">
+                <div>
+                    <h4>Clima actual en Quito</h4>
+                    <span>Temperatura: ${climaActual.temperature}°C</span><br>
+                    <span>Velocidad del viento: ${climaActual.windspeed} km/h</span><br>
+                    <span>Estado del cielo: ${estadoCielo}</span>
+                </div>
+            </div>
+        `;
+        contenedorClima.appendChild(tarjetaClima);      
+    }catch (error) {
+        console.error("error al obtener los datos del clima:", error);
+        mensajeClima.textContent = "No se pudo obtener el clima actual.";
+    }
+}
+btnClima.addEventListener("click", obtenerClimaQuito);
